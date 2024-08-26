66797afab3cae2c3bd7344c754a69f5c803d12ae
LikhithD1 <vinid1409@gmail.com>
2024-08-26 17:13:47 +0530

    Update spiralMatrix.py

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -1,3 +1,19 @@
 res=[]
 left,right=0,len(martix[0])
-top,bottom=0,
+top,bottom=0,len(martrix)
+while left <right and top < buttom:
+  for i in range (left,right):
+    res.append(martix[top][i])
+  top +=1
+   for i in range (top,buttom):
+    res.append(martix[i][right-1])
+   right -=1
+  if not (left < right and top < buttom):
+    break
+   for i in range (right-1,left-1,-1):
+    res.append(martix[bottom-1][i])
+   buttom -=1
+   for i in range (buttom-1,top-1,-1):
+    res.append(martix[i][left])
+  left +=1
+ return res 
